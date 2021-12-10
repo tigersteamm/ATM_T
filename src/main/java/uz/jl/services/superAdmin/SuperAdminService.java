84d026f30f1acbda4db9f326f688a7f023c9e04e
nod1rbek <abdukarimov.inha@gmail.com>
2021-12-10 11:40:45 +0500

    asdasdv ushdfsd

--- src/main/java/uz/jl/services/superAdmin/SuperAdminService.java
+++ src/main/java/uz/jl/services/superAdmin/SuperAdminService.java
@@ -5,10 +5,16 @@ package uz.jl.services.superAdmin;
  */
 public class SuperAdminService {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
     public static void main(String[] args) {
         System.out.println("HIiiiii");
     }
 >>>>>>> f365abae2dde360112c84a18216cd17b0bb9fd6d
+=======
+    public static void main(String[] args) {
+        System.out.println("HIiiiii");
+    }
+>>>>>>> origin/version1
 }
