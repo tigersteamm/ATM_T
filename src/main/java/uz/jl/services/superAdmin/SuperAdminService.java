f365abae2dde360112c84a18216cd17b0bb9fd6d
Your Name <you@example.com>
2021-12-10 11:30:24 +0500

    super admin service

--- src/main/java/uz/jl/services/superAdmin/SuperAdminService.java
+++ src/main/java/uz/jl/services/superAdmin/SuperAdminService.java
@@ -4,5 +4,7 @@ package uz.jl.services.superAdmin;
  * @author Nodirbek Abdukarimov Fri. 11:19 AM. 12/10/2021
  */
 public class SuperAdminService {
-
+    public static void main(String[] args) {
+        System.out.println("HIiiiii");
+    }
 }
