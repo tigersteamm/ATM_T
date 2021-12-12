50e59f8d3df7594de3b8eb72045a96244a99a8cb
D4uranbek <d4uranbek@gmail.com>
2021-12-12 17:08:12 +0500

    menu localized2

--- src/main/java/uz/jl/ui/BranchUI.java
+++ src/main/java/uz/jl/ui/BranchUI.java
@@ -1,7 +1,10 @@
 package uz.jl.ui;
 
+import uz.jl.configs.LangConfig;
+import uz.jl.configs.Session;
 import uz.jl.dao.branch.BranchDao;
 import uz.jl.mapper.BranchMapper;
+import uz.jl.models.settings.Language;
 import uz.jl.response.ResponseEntity;
 import uz.jl.services.branch.BranchService;
 import uz.jl.utils.Input;
@@ -13,24 +16,24 @@ import static uz.jl.utils.Input.getStr;
  * @author Elmurodov Javohir, Wed 12:11 PM. 12/8/2021
  */
 public class BranchUI {
-    // TODO: 09.12.2021 set check for admin
     static BranchService service = BranchService.getInstance(BranchDao.getInstance(), BranchMapper.getInstance());
+    static Language language = Session.getInstance().getUser().getLanguage();
 
     public static void create() {
-        String name = Input.getStr("Branch name: ");
+        String name = Input.getStr(LangConfig.get(language, "branch.name"));
         ResponseEntity<String> response = service.create(name);
         showResponse(response);
     }
 
     public static void update() {
-        String oldName = getStr("Old branch name: ");
-        String newName = getStr("New branch name: ");
+        String oldName = getStr(LangConfig.get(language, "old.branch.name"));
+        String newName = getStr(LangConfig.get(language, "new.branch.name"));
         ResponseEntity<String> response = service.update(oldName, newName);
         showResponse(response);
     }
 
     public static void delete() {
-        String name = Input.getStr("Branch name: ");
+        String name = Input.getStr(LangConfig.get(language, "branch.name"));
         ResponseEntity<String> response = service.delete(name);
         showResponse(response);
     }
@@ -41,14 +44,14 @@ public class BranchUI {
 
     public static void block() {
         unblockList();
-        String name = Input.getStr("Branch name: ");
+        String name = Input.getStr(LangConfig.get(language, "branch.name"));
         ResponseEntity<String> response = service.block(name);
         showResponse(response);
     }
 
     public static void unblock() {
         blockList();
-        String name = Input.getStr("Branch name: ");
+        String name = Input.getStr(LangConfig.get(language, "branch.name"));
         ResponseEntity<String> response = service.unblock(name);
         showResponse(response);
     }
